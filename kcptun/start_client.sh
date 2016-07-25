28759519fb934aa3904949af4386b6f25f4c5f01
ruleless <ruleless@126.com>
2016-07-25 16:39:46 +0800

    kcptun

--- kcptun/start_client.sh
+++ kcptun/start_client.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./client_linux_amd64 -l :12948 -r 127.0.0.1:29900 -key test -mtu 1400 -sndwnd 256 -rcvwnd 2048 -mode fast2 -dscp 46 > client.log 2>&1 &
+./client_linux_amd64 -l :5080 -r 45.63.60.117:29900 -key test -mtu 1400 -sndwnd 256 -rcvwnd 2048 -mode fast2 -dscp 46 > client.log 2>&1 &
